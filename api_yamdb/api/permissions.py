3309f882d6c3f60cf0bf616d61819a9f3271de8b
Александр Селиванов <sasha221200@gmail.com>
2026-01-29 09:11:08 +0300

    Немного переделал поле role  модели кастомного юзера для пермишенов, написал пермишены

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """Пермишн даёт доступ не для чтения только админу"""
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.is_admin
+    
+
+class IsAdminOrModeratorOrAuthor(BasePermission):
+    """Пермишн даёт доступ не для чтения автору, модератору и админу"""
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        if not request.user.is_authenticated:
+            return False
+        
+        return (
+            obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,7 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    role = models.CharField('Роль', max_length=30, blank=True)
+    """Кастомная модель юзера."""
+
+    ADMIN = 'Admin'
+    MODERATOR = 'Moderator'
+    USER = 'User'
+
+    ROLE_CHOISES = (
+        (ADMIN, 'Admin'),
+        (MODERATOR, 'Moderator'),
+        (USER, 'User')
+    )
+
+    role = models.CharField(
+        'Роль',
+        max_length=20,
+        choices=ROLE_CHOISES,
+        default=USER,
+    )
     bio = models.TextField('Биография', blank=True)
     confirmation_code = models.CharField(
         max_length=255,
@@ -17,3 +34,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
