841a2b3aba993f54d389c6c6560571cc886f1e36
FonKeep <fonkeep@mail.ru>
2026-01-29 20:24:53 +0300

    Auth (#13)
    * добавлен ресивер после модели Юзера, для получения кодв на почту
    
    * настройки по почте и времени ждя пароля
    
    ---------
    
    Co-authored-by: Александр Пономарев <slaveofsatan@yandex.ru>

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.tokens import default_token_generator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
@@ -41,3 +44,13 @@ class CustomUser(AbstractUser):
     @property
     def is_moderator(self):
         return self.role == self.MODERATOR
+
+
+@receiver(post_save, sender=CustomUser)
+def set_confirmation_code(sender, instance, created, **kwargs):
+    if created:
+        confirmation_code = default_token_generator.make_token(instance)
+        CustomUser.objects.filter(pk=instance.pk).update(
+            confirmation_code=confirmation_code
+        )
+        instance.confirmation_code = confirmation_code
