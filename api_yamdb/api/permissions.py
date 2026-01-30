6d544c66405e18c12a3e6394818e09395b6639ce
Tikhon Karpukhin <fonkeep@mail.ru>
2026-01-30 15:18:55 +0300

    0.0.3

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,9 +7,7 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        if not request.user.is_authenticated:
-            raise exceptions.NotAuthenticated()
-        return request.user.is_admin or request.user.is_staff
+        return request.user.is_authenticated and (request.user.is_admin or request.user.is_superuser)
     
 
 class IsAdminOrModeratorOrAuthor(BasePermission):
